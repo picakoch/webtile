ef648bd5767702d52d0ec806d64bc793922c928c
Antoine Scherrer <picakoch@gmail.com>
2024-04-07 22:32:21 +0200

    add email

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,1 +1,20 @@
-module.exports = () => ({});
+module.exports = () => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.example.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: 'antoine@n21.fr',
+        defaultReplyTo: 'antoine@n21.fr',
+      },
+    },
+  },
+ });
