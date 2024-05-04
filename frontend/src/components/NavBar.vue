0a2105dd23f96eb1e0b7857f3b801a3c069cc442
Antoine Scherrer <picakoch@gmail.com>
2024-05-05 00:02:54 +0200

    adjust colors

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky" class="uk-background-cover"
-         :style="`background-image: url(${$store.getters.backend_url + $store.getters.config.banner.data.attributes.formats}); height: 90px; width: 100%; opacity: 1`">
+         :style="`background-image: url(${$store.getters.backend_url + $store.getters.config.banner.data.attributes.formats}); height: 100px; width: 100%; opacity: 1`">
       <nav class="uk-navbar-container" uk-navbar style="background: none;">
         <div class="uk-navbar-left uk-margin-left">
           <ul class="uk-navbar-nav">
             <li>
               <RouterLink
-                  to="/all"
-                  class="nav_text_main"
+                  to="/time"
+                  class="nav-text-main"
               >{{ $store.getters.config?.title }}
               </RouterLink>
             </li>
@@ -18,7 +18,7 @@
         <div class="uk-navbar-right uk-margin-right">
           <ul class="uk-navbar-nav">
             <li v-if="sub_categories.length > 0">
-              <a href="#">Aller à</a>
+              <a href="#">Aller ...</a>
               <div class="uk-navbar-dropdown uk-light uk-background-secondary">
                 <ul class="uk-nav uk-navbar-dropdown-nav">
                   <li v-for="(sub_category, index) in sub_categories" :key="index" class="nav-item">
@@ -81,22 +81,22 @@ export default {
 .uk-navbar-nav > li > a {
   font-size: 1.1em;
   font-weight: 600;
-  color: white;
-  padding-left: 10px;
-  padding-right: 10px;
-  border: 5px solid white;
+  color: #ccc;
+  padding-left: 20px;
+  padding-right: 20px;
+  border-bottom: 5px solid #ccc;
   border-color: #00000000;
 }
 
 .uk-navbar-nav > li > a:hover {
-  color: #ccc;
+  color: #fff;
 }
 
 .uk-navbar-nav > li > .uk-active {
-  border-color: #ffffff;
+  border-color: #ccc;
 }
 
-.nav_text_main {
+.nav-text-main {
   font-size: 2.2em !important;
 }
 
