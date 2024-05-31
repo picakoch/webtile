2858036163db7fced170d60b42bc5f30f862f3c2
Antoine Scherrer <picakoch@gmail.com>
2024-05-31 14:48:49 +0200

    add export to excel

---
+++ backend/config/excel.js
@@ -0,0 +1,13 @@
+module.exports = {
+  config: {
+    "api::newsletter-subscription.newsletter-subscription": {
+      columns: [
+        "name",
+        "email",
+        "subscription_date",
+      ],
+      relation: {},
+      locale: "false",
+    },
+  },
+};
