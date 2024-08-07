740c802ead5bedf059db0a2c152ca583db18a839
Antoine Scherrer <picakoch@gmail.com>
2024-08-08 00:12:51 +0200

    add nvm in update

--- update.sh
+++ update.sh
@@ -9,6 +9,11 @@ docker compose up -d
 
 echo "Rebuild frontend"
 cd ../frontend
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+nvm use v18.20.1
+
 yarn
 yarn build
 
