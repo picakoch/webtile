912a3705164ec722c7cfe76919fd1d53cd2e496e
Antoine Scherrer <picakoch@gmail.com>
2026-01-27 23:44:19 +0100

    allow localhost

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -2,7 +2,20 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:4080',
+        'http://127.0.0.1:4080',
+        'https://autourdeminuit-jazz.fr',
+        'https://www.autourdeminuit-jazz.fr',
+      ],
+      headers: ['Content-Type', 'Authorization'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   {
